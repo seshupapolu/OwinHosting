f645078cfc3bff0a07de5ffa4a824ebb24c8d14e
Sesha_Papolu <Sesha_Papolu@EPINHYDW1014>
2017-04-27 16:36:14 +0530

    Added Test Project

---
+++ Owin.Hosting.Demo/Owin.Hosting.Demo.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Owin.Hosting.Demo.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
